fb85fb2f85aa5be8a74cc049cf095181ca446b8a
Rajdip27 <srajdip920@gamil.com>
2025-11-25 22:47:04 +0600

    git psuh

---
+++ src/HostelMealManagement.Application/Repositories/Base/IBaseService.cs
@@ -0,0 +1,153 @@
+﻿using HostelMealManagement.Core.Entities.BaseEntities;
+using HostelMealManagement.Infrastructure.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace HostelMealManagement.Application.Repositories.Base;
+
+internal interface IBaseService<T> where T : AuditableEntity
+{
+    IQueryable<T> All();
+    IQueryable<T> All(params Expression<Func<T, Object>>[] includeProperties);
+    Task<List<T>> GetAllAsync();
+    Task<List<T>> GetAllAsync(params Expression<Func<T, Object>>[] includes);
+    Task<T> FindAsync(long id);
+    Task<T> FindAsync(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includes);
+
+    T Find(long id);
+    T Find(Int64 id, params Expression<Func<T, Object>>[] includeProperties);
+    void Insert(T entity);
+    void Update(T entity, long id);
+    void Delete(T entity);
+
+    Task InsertAsync(T entity);
+    Task UpdateAsync(long id, T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
+}
+public class BaseService<T> : IBaseService<T> where T : AuditableEntity
+{
+    private readonly ApplicationDbContext _context;
+    private DbSet<T> entities;
+    public BaseService(ApplicationDbContext context)
+    {
+        _context = context;
+        entities = _context.Set<T>();
+    }
+    public IQueryable<T> All()
+    {
+        return entities;
+    }
+    public IQueryable<T> All(params Expression<Func<T, Object>>[] includeProperties)
+    {
+        IQueryable<T> queryable = All();
+        foreach (Expression<Func<T, object>> includeProperty in includeProperties)
+        { queryable = queryable.Include<T, object>(includeProperty); }
+        return queryable;
+    }
+    public async Task<List<T>> GetAllAsync()
+    {
+        return await entities.ToListAsync();
+    }
+    public async Task<List<T>> GetAllAsync(params Expression<Func<T, Object>>[] includes)
+    {
+        return await includes.Aggregate(
+            entities.AsQueryable(),
+            (current, include) => current.Include(include)).ToListAsync();
+    }
+    public async Task<T> FindAsync(long id) => await entities.FindAsync(id);
+
+    public async Task<T> FindAsync(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includes)
+    {
+        return await includes.Aggregate(entities.AsQueryable(),
+            (current, include) => current.Include(include),
+            c => c.AsNoTracking().FirstOrDefaultAsync(predicate)
+        ).ConfigureAwait(false);
+    }
+
+
+
+
+    public void Delete(T entity)
+    {
+        if (entity == null)
+        {
+            throw new ArgumentNullException("entity");
+        }
+        entities.Remove(entity);
+        _context.SaveChanges();
+    }
+
+    public T Find(long id)
+    {
+        return entities.SingleOrDefault(s => s.Id == id);
+    }
+
+    public T Find(long id, params Expression<Func<T, object>>[] includeProperties)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Insert(T entity)
+    {
+        if (entity == null)
+        {
+            throw new ArgumentNullException("entity");
+        }
+        entity.CreatedDate = DateTime.UtcNow;
+        entities.Add(entity);
+        _context.SaveChanges();
+    }
+
+    public void Update(T entity, long id)
+    {
+        if (entity == null)
+        {
+            throw new ArgumentNullException("entity");
+        }
+        T exist = _context.Set<T>().Find(id);
+        if (exist != null)
+        {
+            _context.Entry(exist).CurrentValues.SetValues(entity);
+            _context.SaveChanges();
+        }
+        _context.SaveChanges();
+    }
+
+    public async Task UpdateAsync(long id, T entity)
+    {
+        if (entity == null)
+        {
+            throw new ArgumentNullException("entity");
+        }
+        T exist = _context.Set<T>().Find(id);
+        if (exist != null)
+        {
+            _context.Entry(exist).CurrentValues.SetValues(entity);
+            await _context.SaveChangesAsync();
+        }
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(T entity)
+    {
+        entities.Update(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(T entity)
+    {
+        if (entity == null)
+        {
+            throw new ArgumentNullException("entity");
+        }
+        entities.Remove(entity);
+        await _context.SaveChangesAsync();
+    }
+    public async Task InsertAsync(T entity)
+    {
+        entity.CreatedDate = DateTime.UtcNow;
+        await entities.AddAsync(entity);
+        await _context.SaveChangesAsync();
+    }
+}
