f3f836fff9fe83300fc9a2cde4241cb5bfbc51c2
Rajdip27 <srajdip920@gmail.com>
2025-11-27 16:38:28 +0600

    fix issue 🚀🚑

---
+++ src/HostelMealManagement.Application/Helpers/Permissions.cs
@@ -0,0 +1,11 @@
+﻿namespace HostelMealManagement.Application.Helpers;
+
+public static class Permissions
+{
+    public const string Edit = "Admin,Manager";
+    public const string Delete = "Admin";
+    public const string View = "Admin,Manager,Member";
+    public const string Admin = "Admin";
+    public const string Manager = "Manager";
+    public const string Member = "Member";
+}
