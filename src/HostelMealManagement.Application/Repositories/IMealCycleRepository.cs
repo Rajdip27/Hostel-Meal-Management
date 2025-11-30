786b3a69d6a7135c4998993c98dec6ce77f6477d
Rajdip27 <srajdip920@gmail.com>
2025-11-30 12:36:33 +0600

    MealCycle add

---
+++ src/HostelMealManagement.Application/Repositories/IMealCycleRepository.cs
@@ -0,0 +1,13 @@
+﻿using HostelMealManagement.Application.Repositories.Base;
+using HostelMealManagement.Core.Entities;
+using HostelMealManagement.Infrastructure.DatabaseContext;
+
+namespace HostelMealManagement.Application.Repositories;
+
+public interface IMealCycleRepository:IBaseService<MealCycle>
+{
+}
+
+public class MealCycleRepository(ApplicationDbContext context) : BaseService<MealCycle>(context), IMealCycleRepository
+{
+}
