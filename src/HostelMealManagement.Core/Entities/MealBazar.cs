01384a9c79be05d4e2b6398693352a5a397a8b42
Mehedi49423 <marouf384@gmail.com>
2025-12-14 12:43:23 +0600

    add

--- src/HostelMealManagement.Core/Entities/MealBazar.cs
+++ src/HostelMealManagement.Core/Entities/MealBazar.cs
@@ -7,6 +7,14 @@ public class MealBazar : AuditableEntity
     public DateTimeOffset BazarDate { get; set; }
     public decimal BazarAmount { get; set; }
     public string Description { get; set; }=string.Empty;
+    public DateTimeOffset StartDate { get; set; }
+    public DateTimeOffset EndDate { get; set; }
+    public int TotalDayCount { get; set; }
+    //1,2,3
+    //1
+    //2
+    //3
+    public string MealMemberId{ get; set; }=string.Empty;//1,2,3,4,56
     public List<MealBazarItem> Items { get; set; } = new();
 
 }
