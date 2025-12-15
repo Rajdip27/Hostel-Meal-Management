552452ddb272dd1dcca3953c3548b4a02066ab83
Rajdip27 <srajdip920@gmail.com>
2025-12-15 13:35:43 +0600

    fix issue 🚑🚀

---
+++ src/HostelMealManagement.Application/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using NodaTime;
+using System.Text;
+
+namespace HostelMealManagement.Application.Extensions;
+
+public static class DateTimeExtensions
+{
+    private static IHttpContextAccessor _httpContextAccessor;
+
+    public static void Configure(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    public static DateTimeOffset ToUserTime(this DateTimeOffset utc)
+    {
+        var context = _httpContextAccessor?.HttpContext;
+        if (context == null) return utc;
+
+        var ianaTimeZone = context.Session.GetString("UserTimeZone") ?? "UTC";
+
+        try
+        {
+            // Get IANA timezone using NodaTime
+            var tzdb = DateTimeZoneProviders.Tzdb;
+            var zone = tzdb[ianaTimeZone];
+
+            // Convert UTC to user timezone
+            var instant = Instant.FromDateTimeOffset(utc);
+            var zonedDateTime = instant.InZone(zone);
+
+            return zonedDateTime.ToDateTimeOffset();
+        }
+        catch
+        {
+            return utc;
+        }
+    }
+    /// <summary>
+    /// Converts a DateTimeOffset to UTC
+    /// </summary>
+    public static DateTimeOffset ToUtc(this DateTimeOffset dateTime)
+    {
+        return dateTime.ToUniversalTime();
+    }
+}
