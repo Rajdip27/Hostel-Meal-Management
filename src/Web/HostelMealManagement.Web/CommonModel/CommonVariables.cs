27330f2ea7511396df159ba17f3cb4d8d7dba862
Mehedi49423 <marouf384@gmail.com>
2025-12-23 16:42:21 +0600

    Desgin Change.

--- src/Web/HostelMealManagement.Web/CommonModel/CommonVariables.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HostelMealManagement.Application.CommonModel;
-
-public class CommonVariables
-{
-    public const int pageIndex = 0;
-    public const int pageSize = 10;
-    public const int DropdownSize = 1000;
-    public const string ProfileLocation = "images/profiles";
-}
