9d30d6f05d961f81065ad4cab4fd25d2a7a7fe4d
Rajdip27 <srajdip920@gamil.com>
2026-01-03 23:18:34 +0600

    fix iisue

---
+++ src/HostelMealManagement.Infrastructure/Configuration/MealBillConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using HostelMealManagement.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HostelMealManagement.Infrastructure.Configuration;
+
+public class MealBillConfiguration : IEntityTypeConfiguration<MealBill>
+{
+    public void Configure(EntityTypeBuilder<MealBill> builder)
+    {
+        builder.ToTable(nameof(MealBill));
+        builder.HasKey(m => m.Id);
+        builder.HasOne(m => m.Member)
+               .WithMany(b => b.MealBills)
+               .HasForeignKey(m => m.MemberId)
+               .OnDelete(DeleteBehavior.Cascade);
+        builder.HasOne(m => m.MealCycle)
+               .WithMany(b => b.MealBills)
+               .HasForeignKey(m => m.MealCycleId)
+               .OnDelete(DeleteBehavior.Cascade);
+    }
+}
